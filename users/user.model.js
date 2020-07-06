e95cdcc8139758b4552a94ba3ee1410058a94512
Mohan <vrinda.mohan@dxc.com>
2020-07-06 15:29:45 +0530

    added admin checkbox

--- users/user.model.js
+++ users/user.model.js
@@ -7,6 +7,7 @@ const schema = new Schema({
     hash: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
+    admin: { type: Boolean, required: true,default:false },
     createdDate: { type: Date, default: Date.now }
 });
 
